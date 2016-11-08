6789ec109ea1d79b0367076c9bc3e16e37012b31
Justin Harringa <justin@harringa.com>
2016-11-08 01:09:53 -0800

    Show InfoWindow when clicking on a marker

--- src/js/VenueMap.js
+++ src/js/VenueMap.js
@@ -36,6 +36,7 @@ function addMarkerToVenue(venue) {
     venue.marker.addListener('click',
         function() {
             toggleBounce(venue.marker);
+            showInfoWindow(venue);
     });
 }
 
@@ -48,4 +49,32 @@ function toggleBounce(marker) {
     }
     marker.setAnimation(google.maps.Animation.BOUNCE);
     currentlyBouncingMarker = marker;
+}
+
+var currentInfoWindow = null;
+
+// Build the content string for an InfoWindow
+function getVenueInfoWindowString(venue) {
+    var addressToDisplay = venue.address;
+    if (addressToDisplay === null) {
+        addressToDisplay = 'lat: ' + venue.location.lat + ', lng: ' + venue.location.lon;
+    }
+    return '<div class="venueName">' + venue.name + '</div>' +
+        '<div class="venueAddress">' + addressToDisplay + '</div>';
+}
+
+// Derived from Google Maps API doc: https://developers.google.com/maps/documentation/javascript/infowindows#open
+// We only show 1 InfoWindow at a time so close an existing InfoWindow and open a new one for the current venue's
+// marker on the current map associated with the venue.
+function showInfoWindow(venue) {
+    var marker = venue.marker;
+    if (currentInfoWindow !== null) {
+        currentInfoWindow.close();
+    }
+    var contentString = getVenueInfoWindowString(venue);
+    var infoWindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+    infoWindow.open(marker.getMap(), marker);
+    currentInfoWindow = infoWindow;
 }
